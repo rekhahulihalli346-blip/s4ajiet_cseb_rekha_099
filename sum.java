ca20d7cbd54cd8ca3954be00a6ece0bee15a834a
rekhahulihalli346-blip <rekhahulihalli346@gmail.com>
2026-05-07 14:15:35 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+package divide;
+
+public class sum {
+	public static void main(String[] args) {
+		int num=123456789;
+		int sum=0;
+		while(num>0) {
+			int digit=num%10;
+			sum=sum+digit;
+			
+			num=num/10;
+		}
+		System.out.println("sum of digits:"+sum);
+	}
+}
+
+
