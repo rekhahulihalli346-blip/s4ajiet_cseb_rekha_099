b49b4d22d9efd65754197402c10ebceb2b76f25b
rekhahulihalli346-blip <rekhahulihalli346@gmail.com>
2026-05-07 14:12:33 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,19 @@
+package divide;
+
+public class armstrong {
+	 public static void main(String[] args) {
+	int num=153;
+	int temp=num;
+	int sum=0;
+	while(num>0) {
+		int digit=num%10;
+		sum=sum+digit*digit*digit;
+		num=num/10;
+	}
+	if(sum==temp)
+		System.out.println("Armstrong");
+	else 
+		System.out.println("Not armstrong");
+	
+	}
+}
