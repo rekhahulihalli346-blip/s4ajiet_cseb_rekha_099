1624e12f1fe3607f9da48200a29ed897e2ecb52c
rekhahulihalli346-blip <rekhahulihalli346@gmail.com>
2026-05-08 09:38:50 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,9 @@
+package divide;
+ public class arr {
+	 public static void main(String args[]) {
+		 String str="College";
+		 for(int i=str.length()-1;i>=0;i--) {
+			 System.out.print(str.charAt(i));
+		 }
+	 }
+ }
