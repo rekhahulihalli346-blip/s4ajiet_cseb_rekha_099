3ccc645b04284d5b16935ff5cbceda96665037ef
rekhahulihalli346-blip <rekhahulihalli346@gmail.com>
2026-05-08 10:17:31 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+package divide;
+
+public class string {
+	public static void main(String args[]) {
+		
+	StringBuilder q =new StringBuilder() ;
+			q.append("hello") ;
+			q.reverse();
+			String f=q.toString();
+			int l=q.length();
+			System.out.println(f);
+			System.out.println(l);
+}
+}
